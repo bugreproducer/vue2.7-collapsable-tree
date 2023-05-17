d5656824136504663a149454def3408858e0a9cb
Anonymous <developer@localhost>
2023-05-17 11:29:22 +0200

    Added Vue 2.7.3

---
+++ README.md
@@ -0,0 +1,20 @@
+# Vue 2.7.3
+
+This is a basic Git repository with Vue 2.7.3 for testing and demonstration purposes.
+
+## Usage
+
+```bash
+# Clone the repository
+git clone
+
+# Install dependencies
+yarn # or: npm install
+
+# Run the server
+yarn serve # or: npm run serve
+```
+
+## License
+
+> MIT License
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <span class="message">
+      Vue 2.7.3
+    </span>
+  </div>
+</template>
+
+<script>
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  setup() {
+    const a = { a: ref(1) };
+    console.log(a);
+
+    return {
+      a,
+    };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
